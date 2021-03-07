728dc66838ef94e2f896e8dd3ce1435488607176
mnabeshimase <mnabeshimase@mnabeshimas-MacBook-Air.local>
2021-03-07 10:44:25 -0800

    feat: add subscription for createdStory

--- src/Stories.js
+++ src/Stories.js
@@ -1,4 +1,5 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery, useSubscription } from "@apollo/client";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 const LIST_STORIES_BY_ROOM_ID_QUERY = gql`
@@ -9,16 +10,46 @@ const LIST_STORIES_BY_ROOM_ID_QUERY = gql`
   }
 `;
 
+const STORY_CREATED_SUBSCRIPTION = gql`
+  subscription StoryCreated {
+    storyCreated {
+      description
+    }
+  }
+`;
+
 export const Stories = () => {
   const { roomId } = useParams();
   // TODO: handle error state
-  const { data, loading } = useQuery(LIST_STORIES_BY_ROOM_ID_QUERY, {
-    variables: { id: roomId },
-  });
+  const { data: listStoriesByRoomIdData, loading } = useQuery(
+    LIST_STORIES_BY_ROOM_ID_QUERY,
+    {
+      variables: { id: roomId },
+    }
+  );
+  const { data: StoryCreatedData } = useSubscription(
+    STORY_CREATED_SUBSCRIPTION
+  );
+  const [stories, setStories] = useState([]);
+
+  useEffect(() => {
+    if (listStoriesByRoomIdData) {
+      const { listStoriesByRoomId } = listStoriesByRoomIdData;
+      setStories(listStoriesByRoomId);
+    }
+  }, [listStoriesByRoomIdData]);
+
+  useEffect(() => {
+    if (StoryCreatedData) {
+      const { storyCreated } = StoryCreatedData;
+      setStories([...stories, storyCreated]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [StoryCreatedData]);
 
   if (loading) {
     return <div>Loading</div>;
   }
 
-  return <div>{JSON.stringify(data)}</div>;
+  return <div>{JSON.stringify(stories)}</div>;
 };
