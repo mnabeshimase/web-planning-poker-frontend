ee06d5184e78e385f312702564719155979c91ab
mnabeshimase <mnabeshimase@mnabeshimas-MacBook-Air.local>
2021-03-07 13:55:32 -0800

    feat: create vote for current story

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,45 @@
-import { gql, useMutation } from "@apollo/client";
+import { gql, useMutation, useQuery, useSubscription } from "@apollo/client";
 import { Button } from "baseui/button";
 import { useParams } from "react-router";
 
 const UPSERT_VOTE_MUTATION = gql`
   mutation UpsertVote($upsertVoteInput: UpsertVoteInput!) {
     upsertVote(upsertVoteInput: $upsertVoteInput) {
-      roomId
+      storyId
       userId
       score
     }
   }
 `;
 
+const ROOM_QUERY = gql`
+  query Room($id: ID!) {
+    room(id: $id) {
+      currentStoryId
+    }
+  }
+`;
+
+const ROOM_UPDATED_SUBSCRIPTION = gql`
+  subscription RoomUpdated {
+    roomUpdated {
+      currentStoryId
+    }
+  }
+`;
+
 export const Card = (props) => {
-  const { value, userId, isSelected, setSelectedCard } = props;
   const { roomId } = useParams();
+  const { value, userId, isSelected, setSelectedCard } = props;
+  const { data: roomData } = useQuery(ROOM_QUERY, {
+    variables: { id: roomId },
+  });
   const [upsertVote] = useMutation(UPSERT_VOTE_MUTATION);
+  const { data: roomUpdatedData } = useSubscription(ROOM_UPDATED_SUBSCRIPTION);
+
+  const storyId =
+    roomUpdatedData?.roomUpdated.currentStoryId ||
+    roomData?.room.currentStoryId;
 
   return (
     <Button
@@ -23,7 +47,7 @@ export const Card = (props) => {
       onClick={() => {
         setSelectedCard(value);
         upsertVote({
-          variables: { upsertVoteInput: { userId, roomId, score: value } },
+          variables: { upsertVoteInput: { userId, storyId, score: value } },
         });
       }}
       overrides={{
