6d821b0ce36c3a2316d953ca21b2f06e9afde0de
mnabeshimase <mnabeshimase@mnabeshimas-MacBook-Air.local>
2021-03-06 20:23:18 -0800

    feat: fetch votes

--- src/Votes.js
+++ src/Votes.js
@@ -1,3 +1,25 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router";
+
+const LIST_VOTES_BY_ROOM_ID_QUERY = gql`
+  query ListVotesByRoomId($id: ID!) {
+    listVotesByRoomId(id: $id) {
+      userId
+      score
+    }
+  }
+`;
+
 export const Votes = () => {
-  return <div>Votes</div>;
+  const { roomId } = useParams();
+  // TODO: handle error state
+  const { data, loading } = useQuery(LIST_VOTES_BY_ROOM_ID_QUERY, {
+    variables: { id: roomId },
+  });
+
+  if (loading) {
+    return <div>loading</div>;
+  }
+
+  return <div>{JSON.stringify(data.listVotesByRoomId)}</div>;
 };
